382e36ff259888f8e8e2b65d0182ec25a0fe977d
Antonio Garza <amgarza35@amarillocollege.com>
2025-03-18 14:01:31 -0500

    make some change

--- Main.java
+++ Main.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] var0) {
-        System.out.println("Hello world");
-    
+        //System.out.println("Hello world");
+
+        //Scanner myObj = new Scanner(System.in);
+        //System.out.println("Enter username");
+
+        //String userName = myObj.nextLine();
+        //System.out.println("Username is: " + userName);
+
+        //System.out.println("What year were you born in?");
+
+        //int year = myObj.nextInt();
+
+        //int age = 2025 - year;
+        //System.out.println("you are " + age + "Years old.");
+
+        String decimal = "3.45";
+
+        Double double1 = Double.parseDouble(decimal);
+        String integer = "4567";
+
+        int int1 = Integer.parseInt(integer);
+
+        System.out.println("double number " +double1);
+        System.out.println("int number " +int1);
+
+
+
 }
 }
